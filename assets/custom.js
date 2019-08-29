2dfb3b3697c515b511274078d5b39f5c02140a0b
rahul1310 <rahul@shoptrade.co>
2019-08-29 19:56:37 +0530

    working on the best-memo section

--- assets/custom.js
+++ assets/custom.js
@@ -173,3 +173,35 @@ function addAllItemsToCart(itemArray, form) {
     };
     Shopify.moveAlong();
 }
+
+// Best memories magnific video starts here
+
+$('.js-best-memo-video').magnificPopup({
+type: 'iframe',
+
+iframe: {
+    patterns: {
+        dailymotion: {
+        
+        index: 'dailymotion.com',
+        
+        id: function(url) {        
+            var m = url.match(/^.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
+            if (m !== null) {
+                if(m[4] !== undefined) {
+                    
+                    return m[4];
+                }
+                return m[2];
+            }
+            return null;
+        },
+        
+        src: 'https://www.dailymotion.com/embed/video/%id%'
+        
+        }
+    }
+}
+
+});
+// Best memories magnific video ends here
